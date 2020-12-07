792bba46eef99efe77f490fef2a78a16b5c3d901
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2020-12-07 09:39:41 -0500

    Updated numbers in array of js file, added notes, testing for loop.

--- script.js
+++ script.js
@@ -17,16 +17,26 @@ generateBtn.addEventListener("click", writePassword);
 
 // where new code starts 120620//
 
+// Setting up arrays for if statements //
+
 var upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var numbers = [1,2,3,4,5,6,7,8,9,0];
 var specChar = ["!","@","#","$","%","^","&","*","~","(",")","-","+"];
 var selUpper;
 var selLower;
 var selNum;
 var selChar;
 
-var passwordLG = prompt("Please confirm a password length - 8 minimum to 128 maximum");
+// testing variable for for loop "
+
+var characters = ["A","B","C","D","E","F","G","H","I","J","K"]
+
+// Prompt message asking for a password length. Description of how many numbers allowed in message. //
+
+var passwordLG = prompt("Please confirm a password length - 8 minimum to 128 maximum"); 
+
+// If statement 
 
 if (passwordLG < 8 ){
   alert("Please enter a password that has at least 8 characters");
@@ -43,23 +53,13 @@ else{
   var selChar = confirm("Do you want special characters?");
 }
 
-if(selUpper){
-  for (var i=0; i< passwordLG; i++){
-    var randomNum = Math.floor((Math.random()*upCase.length));
-    console.log(randomNum);
+// testing for loop in console //
+if(selUpper){ // push all the UpperCase Letters into an array.
+  for (var k=0; k< passwordLG; k++){    
+    console.log(characters);
     
   }
 
 }
-
-
-
-// function example //
-function adder(x,y,z){
-  console.log(x+y+z);
-}
-
-// calls function //
-adder(1,2,3);
 
 
