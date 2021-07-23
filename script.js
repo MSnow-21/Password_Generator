a89f75afb058a165a3d350f33de5e0dc77a04fdd
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-07-23 15:47:15 -0400

    Updated styling on h1 tag in CSS. Changed all vars to let

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -19,30 +19,30 @@ function generatePassword(){
 
   // Arrays for for-loops //
   
-  var upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var numbers = [1,2,3,4,5,6,7,8,9,0];
-  var specChar = ["!","@","#","$","%","^","&","*","~","(",")","-","+"];
+  let upCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let lowCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let numbers = [1,2,3,4,5,6,7,8,9,0];
+  let specChar = ["!","@","#","$","%","^","&","*","~","(",")","-","+"];
   
   // variables for prompts //
   
-  var upperSelected;
-  var lowerSelected;
-  var numberSelected;
-  var characterSelected;
+  let upperSelected;
+  let lowerSelected;
+  let numberSelected;
+  let characterSelected;
   
   // Output arrays for each if statement //
   
-  var characterUpdate = [];
+  let characterUpdate = [];
 
   // Output of array for characterUpdate using original password length //
   // This is required otherwise all "true" selected variables show password length qty //
 
-  var combinedPassword = [];
+  let combinedPassword = [];
   
   // Prompt message asking for a password length. Description of how many numbers allowed in message. //
   
-  var passwordLG = prompt("Please confirm a password length - 8 minimum to 128 maximum"); 
+  let passwordLG = prompt("Please confirm a password length - 8 minimum to 128 maximum"); 
   
   // If statement 
   
@@ -55,18 +55,18 @@ function generatePassword(){
   }
   
   else{
-    var upperSelected = confirm("Do you want capital letters?");
-    var lowerSelected = confirm("Do you want lowercase letters?");
-    var numberSelected = confirm("Do you want numbers?");
-    var characterSelected = confirm("Do you want special characters?");
+    upperSelected = confirm("Do you want capital letters?");
+    lowerSelected = confirm("Do you want lowercase letters?");
+    numberSelected = confirm("Do you want numbers?");
+    characterSelected = confirm("Do you want special characters?");
   }
     
   // UpperCase Letters in characterUpdate Array 
   
   if(upperSelected){ 
-    for (var k=0; k < passwordLG; k++){
-      var calcNum = Math.floor(Math.random()*upCase.length);
-      var upperLetters = upCase[calcNum];
+    for (let k=0; k < passwordLG; k++){
+      let calcNum = Math.floor(Math.random()*upCase.length);
+      let upperLetters = upCase[calcNum];
       characterUpdate.push(upperLetters);
       console.log(upperLetters);
     }
@@ -75,9 +75,9 @@ function generatePassword(){
   // Lowercase letters in characterUpdate Array 
   
   if(lowerSelected){ 
-    for (var k=0; k < passwordLG; k++){
-      var calcNum = Math.floor(Math.random()*lowCase.length);
-      var lowerLetters = lowCase[calcNum];
+    for (let k=0; k < passwordLG; k++){
+      let calcNum = Math.floor(Math.random()*lowCase.length);
+      let lowerLetters = lowCase[calcNum];
       characterUpdate.push(lowerLetters);
       console.log(lowerLetters);
     }
@@ -86,9 +86,9 @@ function generatePassword(){
   // Numbers into characterUpdate Array
 
   if(numberSelected){ 
-    for (var k=0; k < passwordLG; k++){
-      var calcNum = Math.floor(Math.random()*numbers.length);
-      var numberValues = numbers[calcNum];
+    for (let k=0; k < passwordLG; k++){
+      let calcNum = Math.floor(Math.random()*numbers.length);
+      let numberValues = numbers[calcNum];
       characterUpdate.push(numberValues);
       console.log(numberValues);
     }
@@ -97,9 +97,9 @@ function generatePassword(){
   // Characters into characterUpdate Array
 
   if(characterSelected){
-    for (var k=0; k < passwordLG; k++){
-      var calcNum = Math.floor(Math.random()*specChar.length);
-      var characterValues = specChar[calcNum];
+    for (let k=0; k < passwordLG; k++){
+      let calcNum = Math.floor(Math.random()*specChar.length);
+      let characterValues = specChar[calcNum];
       characterUpdate.push(characterValues);
       console.log(characterValues);
     }
@@ -108,9 +108,9 @@ function generatePassword(){
   // In this for loop, the combined password from all the "true" selected promps is ... //
   // reentered utilizing defined password length. It outputs to the combinedPassword array //
 
-  for (var k=0; k < passwordLG; k++){
-    var calcNum = Math.floor(Math.random()*characterUpdate.length);
-    var finalCharacterValues = characterUpdate[calcNum];
+  for (let k=0; k < passwordLG; k++){
+    let calcNum = Math.floor(Math.random()*characterUpdate.length);
+    let finalCharacterValues = characterUpdate[calcNum];
     combinedPassword.push(finalCharacterValues);
     console.log(finalCharacterValues);
     
